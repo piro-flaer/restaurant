990b32f48d84a82a5628881596bea8b122d2a627
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 10:31:11 +0530

    Update UserLocationFunction.jsx

--- src/components/js/UserLocationFunction.jsx
+++ src/components/js/UserLocationFunction.jsx
@@ -1,19 +1,48 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const UserLocationFunction = () => {
-  const [currLocation, setCurrLocation] = useState();
+const UserLocationFunctionCity = () => {
+  const [currLocationCity, setCurrLocationCity] = useState();
 
   useEffect(() => {
     getLocation();
   }, []);
 
   const getLocation = async () => {
-    const location = await axios.get("https://ipapi.co/city/");
-    setCurrLocation(location);
-  };
+    const permissionStatus = await navigator.permissions.query({
+      name: "geolocation",
+    });
+    permissionStatus.state === "granted" &&
+      (async () => {
+        const locationCity = await axios.get("https://ipapi.co/city/");
+        setCurrLocationCity(locationCity.data.toUpperCase());
+      })();
+    };
 
-  return currLocation;
+  return currLocationCity;
 };
 
-export default UserLocationFunction;
+export default UserLocationFunctionCity;
+
+const UserLocationFunctionCoords = () => {
+  const [currLocationCoords, setCurrLocationCoords] = useState();
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    const permissionStatus = await navigator.permissions.query({
+      name: "geolocation",
+    });
+    permissionStatus.state === "granted" &&
+      (async () => {
+        const locationCoords = await axios.get("https://ipapi.co/latlong/");
+        setCurrLocationCoords(locationCoords.data);
+      })();
+    };
+
+  return currLocationCoords;
+};
+
+export { UserLocationFunctionCoords };
