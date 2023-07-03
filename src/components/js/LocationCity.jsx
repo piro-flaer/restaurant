67800abc1b93e4cf75e6e26c305f9119ba445231
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 10:23:28 +0530

    Update LocationCity.jsx

--- src/components/js/LocationCity.jsx
+++ src/components/js/LocationCity.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/LocationStateOpened.css";
 
-const LocationCity = ({ cityValue }) => {
+const LocationCity = ({ cityValue, cityName }) => {
   const [selected, setselected] = useState(false);
+
+  useEffect(() => {
+    document.querySelector(".selected")
+      ? cityName(document.querySelector(".selected").innerHTML)
+      : cityName("");
+  }, [selected]);
+
   return (
     <div
       className={selected ? "locationcity selected" : "locationcity"}
       onClick={() => {
+        !selected &&
+          (() => {
+            document.querySelector(".selected") &&
+              document.querySelector(".selected").classList.remove("selected");
+          })();
+
         setselected(!selected);
       }}
     >
