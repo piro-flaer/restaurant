2fa43bdd28bd111c909cafb758ba4aae4044de1c
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 10:27:46 +0530

    Update MenuApp.jsx

--- src/components/js/MenuApp.jsx
+++ src/components/js/MenuApp.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
-import RotatingDisc from "./RotatingDisc";
+import React, { useEffect, useState } from "react";
+import RotateDiscAngle from "./RotateDiscAngle";
+import RotatingPlate from "./RotatingPlate";
 import MenuDetails from "./MenuDetails";
+import { menuItemKeyString } from "./RotatingItems";
 
 const MenuApp = () => {
+  const radius = 16;
+  const angle = 360 / 6;
   const [menuType, setMenuType] = useState("Burger");
+  const [rotationAngle, setRotationAngle] = useState(0);
+
+  useEffect(() => {
+    setMenuType(menuItemKeyString);
+  }, [menuItemKeyString]);
+
   return (
     <>
-      <RotatingDisc setMenuType={setMenuType} />
+      <RotateDiscAngle setRotationAngle={setRotationAngle} />
+      <RotatingPlate
+        radius={radius}
+        angle={angle}
+        rotationAngle={rotationAngle}
+      />
       <MenuDetails menuTypeId={menuType} />
     </>
   );
