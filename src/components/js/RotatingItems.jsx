dd8c9fa459fc66e21f04179f345a6ae0a3563103
piro-flaer <138122440+piro-flaer@users.noreply.github.com>
2023-07-03 10:29:39 +0530

    Update RotatingItems.jsx

--- src/components/js/RotatingItems.jsx
+++ src/components/js/RotatingItems.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { itemTypes } from "./ItemType";
 
-const RotatingItems = ({ setMenuType, radius, angle, rotationAngle }) => {
+let menuItemKeyString;
+
+const RotatingItems = ({ radius, angle, rotationAngle }) => {
+  const [menuItemKey, setMenuItemKey] = useState("Burger");
+
   const updatedItemTypes = itemTypes.map((item) => {
     let itemAngle = item.id * angle;
     let x = radius * Math.cos((itemAngle + rotationAngle) * (Math.PI / 180));
@@ -15,20 +19,28 @@ const RotatingItems = ({ setMenuType, radius, angle, rotationAngle }) => {
           position: "absolute",
           top: `${y}rem`,
           left: `${x}rem`,
-          fontSize: x === 16 ? "7.5rem" : "5rem",
-          animation: x === 16 ? "vibrate 0.5s ease-out" : "",
+          fontSize: x > 15.8 && x < 16.2 ? "7.5rem" : "5rem",
+          animation: x > 15.8 && x < 16.2 ? "vibrate 0.5s ease-out" : "",
         }}
       ></i>
     );
   });
 
   let menuType = updatedItemTypes
-    .filter((item) => parseInt(item.props.style.left) === 16)
-    .find((item) => true)?.key;
-  if (menuType) {
-    setMenuType(menuType);
-  }
+    .filter((item) => item.props.style.fontSize === "7.5rem")
+    .find((item) => true)
+    ? updatedItemTypes
+        .filter((item) => item.props.style.fontSize === "7.5rem")
+        .find((item) => true).key
+    : menuItemKey;
+
+  useEffect(() => {
+    setMenuItemKey(menuType);
+  }, [menuType]);
+  menuItemKeyString = menuItemKey;
+
   return <div className="itemholder">{updatedItemTypes}</div>;
 };
 
+export { menuItemKeyString };
 export default RotatingItems;
